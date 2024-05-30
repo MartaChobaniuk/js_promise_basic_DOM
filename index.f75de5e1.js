cfcc81965f2eae8d16e8fdb472718200dbbaf12a
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-05-30 15:05:47 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const logo = document.querySelector(".logo");
+const divElem = document.createElement("div");
+const promise1 = new Promise((resolve)=>{
+    logo.addEventListener("click", resolve);
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(reject, 3000);
+});
+const handleSuccess = ()=>{
+    divElem.className = "message";
+    divElem.textContent = "Promise was resolved!";
+    document.body.appendChild(divElem);
+};
+const handleError = ()=>{
+    divElem.className = "message error-message";
+    divElem.textContent = "Promise was rejected!";
+    document.body.appendChild(divElem);
+};
+promise1.then(handleSuccess).catch(handleError);
+promise2.then(handleSuccess).catch(handleError);
+
+//# sourceMappingURL=index.f75de5e1.js.map
